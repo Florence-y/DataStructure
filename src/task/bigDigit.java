213f829ad00bb6cf477be4f4bbfd86f6595bbf95
Florence_y <2213576511@qq.com>
2020-09-20 08:07:00 +0800

    first commit

---
+++ src/task/bigDigit.java
@@ -0,0 +1,81 @@
+package task;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static until.FlorenceUntil.print;
+
+/**
+ * @author Florence
+ */
+public class bigDigit {
+    public static void main(String[] args) {
+        Scanner getData = getScanner();
+        long startTime = System.currentTimeMillis();
+        //do something
+        String result = bigIntegerMultiply(getData.next(),getData.next());
+        print(result);
+        long endTime = System.currentTimeMillis();
+        print("运行："+(endTime-startTime)+"ms");
+    }
+
+    private static String bigIntegerMultiply(String num1, String num2) {
+        //0*anything is 0
+        if("0".equals(num1)|| "0".equals(num2)){
+            return "0";
+        }
+        StringBuilder result = new StringBuilder();
+        //get the reverse Int Arr from the String
+        Integer[] numBit1 = inverseArr(charArrToNumArr(num1.toCharArray()));
+        Integer[] numBit2 = inverseArr(charArrToNumArr(num2.toCharArray()));
+        int[] resultArr = new int[numBit1.length+num2.length()];
+        Arrays.fill(resultArr,0);
+        int carryBit=0;
+        int resultLength=resultArr.length;
+        //key step
+        for(int i=0;i<numBit1.length;i++){
+            for(int j=0;j<numBit2.length;j++){
+                resultArr[i+j]+=numBit1[i]*numBit2[j];
+            }
+        }
+        //carry bit
+        for(int i=0;i<resultLength;i++){
+            int nowNum=resultArr[i]+carryBit;
+            resultArr[i] =nowNum%10;
+            carryBit=nowNum/10;
+        }
+        //translate Int to String
+        for (int i=0;i<resultLength-1;i++){
+            result.append(resultArr[i]);
+        }
+        //the special last step
+        if(resultArr[resultLength-1]!=0){
+            result.append(resultArr[resultArr.length-1]);
+        }
+        //StringBulider to String
+        return result.reverse().toString();
+    }
+
+    private static Integer[] charArrToNumArr(char[] chars) {
+        Integer[] result = new Integer[chars.length];
+        for(int i=0;i<chars.length;i++){
+            result[i]=(int)chars[i]-'0';
+        }
+        return result;
+    }
+
+    private static <T> T[] inverseArr(T[] object){
+        int left=0,right=object.length-1;
+        while(left<right){
+            T temp = object[left];
+            object[left]=object[right];
+            object[right]=temp;
+            left++;
+            right--;
+        }
+        return object;
+    }
+    public static Scanner getScanner(){
+        return new Scanner(System.in);
+    }
+}
---
+++ src/until/FlorenceUntil.java
@@ -0,0 +1,109 @@
+package until;
+
+
+import java.util.Arrays;
+
+/**
+ * @author Florence
+ */
+public class FlorenceUntil {
+    public static void print(Object object){
+        System.out.println(object);
+    }
+
+    /**
+     * 冒泡排序
+     * @param arrs 要排序的数组
+     */
+    public static void bubbleSort(int[] arrs) {
+        for(int i=0;i<arrs.length;i++) {
+            for(int j=0;j<arrs.length-i-1;j++) {
+                if(arrs[j]>arrs[j+1]) {
+                    swap(arrs,j,j+1);
+                }
+            }
+        }
+    }
+
+    /**
+     * 交换两个下标的值
+     * @param sortArrays 要交换的数组地址
+     * @param index1 下标一
+     * @param index2 下标二
+     */
+    public static void swap(int[] sortArrays,int index1,int index2) {
+        int temp=sortArrays[index1];
+        sortArrays[index1]=sortArrays[index2];
+        sortArrays[index2]=temp;
+    }
+    /**
+     * 获取一维测试数组
+     * @param n 上限
+     * @return 得到的数组
+     */
+    public static int[] getOneDimensionNumArr(int begin,int n){
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]=begin+i+1;
+        }
+        return arr;
+    }
+
+    /**
+     * 获取二维数组
+     * @param n 要的大小
+     * @return
+     */
+    public static int[][] getTwoDimensionNumArr(int begin,int n){
+        int[][] arr = new int[n][n];
+        for (int i=0;i<n;i++){
+            arr[i]=getOneDimensionNumArr(n*i+begin,n);
+        }
+        return arr;
+    }
+
+    /**
+     * 获取三维数组 n*n*n
+     * @param n
+     * @return 获取到的三维数组
+     */
+    public static int[][][] getThreeDimensionNumArr(int n){
+        int[][][] arr = new int[n][n][n];
+        for (int i=0;i<n;i++){
+            arr[i]=getTwoDimensionNumArr(n*n*i,n);
+        }
+        return arr;
+    }
+    /**
+     * 展示我们的二维数组
+     * @param arr 展示的二维数组
+     */
+    public static void showTwoDimensionArr(int[][] arr){
+        for (int[] subArr:arr){
+            System.out.println(Arrays.toString(subArr));
+        }
+        System.out.println("**************************************************");
+    }
+
+    /**
+     * 展示三维数组
+     * @param arr
+     */
+    public static void showThreeDimension(int[][][] arr){
+        for (int[][] subArr:arr){
+            showTwoDimensionArr(subArr);
+        }
+    }
+    public static int[][] copyArrTwoDimension(int[][] sourceArr,int[][] wantToCopyArr){
+        for (int i=0;i<sourceArr.length;i++){
+            System.arraycopy(sourceArr[i],0,wantToCopyArr[i],0,sourceArr[i].length);
+        }
+        return wantToCopyArr;
+    }
+    public static int[][][] copyArrThreeDimension(int [][][] sourceArr,int[][][] wantToCopyArr){
+        for (int i=0;i<sourceArr.length;i++){
+            copyArrTwoDimension(sourceArr[i],wantToCopyArr[i]);
+        }
+        return wantToCopyArr;
+    }
+}
